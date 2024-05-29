47cd8556eb7616c9a29537387bc18f2c307bdc35
Kaique Ricardo de Melo <kaikricardo99@gmail.com>
2024-05-28 21:32:43 -0300

    end project structure

--- index.js
+++ index.js
@@ -8,7 +8,51 @@ const app = express();
 
 const conn = require("./db/conn");
 
+app.engine("handlebars", exphbs);
+app.set("view engine", "handlebars");
 
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(express.json());
+
+// session middleware
+app.use(
+  session({
+    name: "session",
+    secret: "my_secret",
+    resave: false,
+    saveUninitialized: false,
+    store: new FileStore({
+      logFn: () => {},
+      path: require("path").join(require("os").tmpdir(), "sessions"),
+    }),
+    cookie: {
+      secure: false,
+      maxAge: 360000,
+      expires: new Date(Date.now() + 360000),
+      httpOnly: true,
+    },
+  })
+);
+
+// flash messages
+app.use(flash());
+
+// public path
+app.use(express.static("public"));
+
+// set session to res
+app.use((req, res, next) => {
+  if (req.session.useId) {
+    res.locals.session = req.session;
+  }
+
+  next();
+});
 
 conn
   .sync()
