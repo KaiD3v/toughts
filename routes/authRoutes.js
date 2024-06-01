10632602811d161a2b5069a88040affe16e58766
Kaique Ricardo de Melo <kaikricardo99@gmail.com>
2024-06-01 16:24:06 -0300

    flash messages

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const AuthController = require("../controllers/AuthController");
 
 router.get("/login", AuthController.login);
+router.post("/register", AuthController.registerPost);
 router.get("/register", AuthController.register);
 
 module.exports = router;
